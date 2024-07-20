ed6edb60857c92d7fcc77c4a862e9c67fd7b1b7e
Luka Đorđević <lukadordevi@gmail.com>
2024-07-20 23:50:41 +0200

    j

--- api.js
+++ api.js
@@ -1,22 +1,26 @@
 const annasApi = {
     generateLink: async (md5, key) => {
         try {
-            let apiResponse = await fetch(`https://annas-archive.li/dyn/api/fast_download.json?md5=${md5}&key=${key}`)
-            console.log(await apiResponse)
+            const apiResponse = await fetch(`https://annas-archive.li/dyn/api/fast_download.json?md5=${md5}&key=${key}`);
+            const jsonApiResponse = await apiResponse.json();
 
-            let jsonApiResponse = await apiResponse.json()
-
-            let responseLink = await jsonApiResponse.download_url
-            let responseInfo = await jsonApiResponse.account_fast_download_info
-    
-            const result = {
-                link: await responseLink,
-                info: await responseInfo
+            if (jsonApiResponse.error) {
+                console.log('Annas Archive API error:', jsonApiResponse.error);
+                return null;
             }
-    
-            return result
+
+            const responseLink = jsonApiResponse.download_url;
+            const responseInfo = jsonApiResponse.account_fast_download_info;
+
+            const result = {
+                link: responseLink,
+                info: responseInfo
+            };
+
+            return result;
         } catch (error) {
-            console.log('Annas Archive API error:', error)
+            console.log('Annas Archive API error:', error);
+            return null;
         }
     }
 };
